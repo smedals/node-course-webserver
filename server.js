cf62f0cb98211814aa260b6e43225efb0cd4f619
Medals <steven.medals@hpe.com>
2018-01-16 15:01:14 -0600

    Add projects page

--- server.js
+++ server.js
@@ -48,6 +48,12 @@ app.get('/about', (req, res) => {
 	});
 });
 
+app.get('/projects', (req, res) => {
+	res.render('projects.hbs', {
+		pageTitle: 'Portfolio Page',
+	});
+});
+
 app.listen(port, () => {
 	console.log(`server is up on port ${port}`);
 });
